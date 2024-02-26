4233f3bdbc054329270ea1ccbf4b8936c5893436
B Praveen Nayak <praveenn@testvagrant.com>
2024-02-26 13:17:17 +0530

    logout addede

--- test/specs/logOut.spec.js
+++ test/specs/logOut.spec.js
@@ -1,13 +1,13 @@
 const { expect } = require('chai');
 const LoginPage = require('../screenobjects/loginPage')
 const OtpPage = require('../screenobjects/otpPage');
-const HomePage = require('../screenobjects/HomePage');
+//const HomePage = require('../screenobjects/HomePage');
+const LogOut = require('../screenobjects/logOutPage');
 
 describe('My Login application', () => {
-    it('should login with valid credentials', async () => {
+    it('should login and logout', async () => {
         await LoginPage.login('ulshopify@ultralesson.com', '12345');
         await OtpPage.enterOtp('0000');
-        const userNameText = await (await HomePage.getUserNameEle()).getText();
-        expect(userNameText).to.equal('Jack Sparrow');
+        await LogOut.logout();     
     })
 })
